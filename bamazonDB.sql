039a0bf1b33ac8186c7a5fe1a06c53dfefb39a6a
vilagen <vilagen@yahoo.com>
2019-07-06 09:14:43 -0400

    Wrote SQL file with database and values

---
+++ bamazonDB.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS bamazon_db;
+
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE  products(
+    item_id INTEGER(12) AUTO_INCREMENT NOT NULL,
+    product VARCHAR(150),
+    department_name VARCHAR(150),
+    price INTEGER(12,2),
+    stock_quantity INTEGER,
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product, department_name, price, stock_quantity)
+VALUES ("Nintendo Switch", "Gaming", 298.99, 25), ("AMD Processor", "Computer", 199.00, 8),
+("Gray Shoes", "Clothing", 29.95, 10), ("Fan", "Home", 15.50, 50), ("Cool Wallet", "Fashion", 19.95, 18),
+("Air Mattress", "Home", 69.95, 5), ("Computer Desk", "Home", 109.85, 3),
+("Super Jump 2", "Video Games", 59.99, 45), ("Headphones", "Electronics", 24.99, 12),
+("HD TV", "Televisions", 359.98, 7), ("Guitar", "Musical Instruments",  149.99, 9)
