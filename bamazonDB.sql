29ebf334af79b45bcb1754c6f8a88d11ea9ad503
vilagen <vilagen@yahoo.com>
2019-07-06 19:20:02 -0400

    created sql database

--- bamazonDB.sql
+++ bamazonDB.sql
@@ -8,7 +8,7 @@ CREATE TABLE  products(
     item_id INTEGER(12) AUTO_INCREMENT NOT NULL,
     product VARCHAR(150),
     department_name VARCHAR(150),
-    price INTEGER(12,2),
+    price DECIMAL(12,2),
     stock_quantity INTEGER,
     PRIMARY KEY (item_id)
 );
@@ -18,4 +18,6 @@ VALUES ("Nintendo Switch", "Gaming", 298.99, 25), ("AMD Processor", "Computer", 
 ("Gray Shoes", "Clothing", 29.95, 10), ("Fan", "Home", 15.50, 50), ("Cool Wallet", "Fashion", 19.95, 18),
 ("Air Mattress", "Home", 69.95, 5), ("Computer Desk", "Home", 109.85, 3),
 ("Super Jump 2", "Video Games", 59.99, 45), ("Headphones", "Electronics", 24.99, 12),
-("HD TV", "Televisions", 359.98, 7), ("Guitar", "Musical Instruments",  149.99, 9)
+("HD TV", "Televisions", 359.98, 7), ("Guitar", "Musical Instruments",  149.99, 9);
+
+SELECT * FROM bamazon_db
